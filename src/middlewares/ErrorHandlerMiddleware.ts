0a1cbb392509fbd3d8c56a00b68928e2e91040e6
ArtyomFahradyan <artyom.fahradyan9554@gmail.com>
2023-07-29 14:10:22 +0400

    Adding ErrorHandlerMiddleware.ts

---
+++ src/middlewares/ErrorHandlerMiddleware.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { ServiceUnavailable } from "errors";
+import { BAD_REQUEST_CODE } from "constatnts";
+
+class ErrorHandlerMiddleware {
+    static async handler(
+        err: Error & { status: number; errors: any },
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        if (!err.status) {
+            next(new ServiceUnavailable(err.message));
+        }
+
+        let status = err.status || BAD_REQUEST_CODE;
+
+        return res.status(status).json({
+            status: status,
+            data: null,
+            message: err.message || "",
+            errors: err.errors || null,
+            body: req.body,
+        });
+    }
+}
+
+export default ErrorHandlerMiddleware;
